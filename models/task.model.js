b3c5e09ae63d2c79057f13f9bb3d9fb68ef3bca1
nathgsa <120767484+nathgsa@users.noreply.github.com>
2025-08-31 21:47:56 +0800

    Add files via upload

---
+++ models/task.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: [true, 'Please provide a title for the task'],
+        trim:true
+    },
+    description:{
+        type:String,
+        required: [true, 'Please provide a discription for the task'],
+        trim:true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'in-progress', 'completed'],
+        default: 'pending',
+        required: true
+    }
+}, {timestamps:true}) /**this will automatically adds creadted_at and udpated_at */
+
+module.exports = mongoose.model('Task', TaskSchema)
